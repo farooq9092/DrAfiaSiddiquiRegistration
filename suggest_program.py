be13636eaea82936b59738e9dadc4674685f126d
Muhammad Farooq <88403419+farooq9092@users.noreply.github.com>
2024-06-12 15:29:21 +0500

    Update suggest_program.py

--- suggest_program.py
+++ suggest_program.py
@@ -52,6 +52,20 @@ def main():
             border-radius: 5px;
             transform: translateY(-50%);
         }
+        .social-icons {
+            position: fixed;
+            top: 50%;
+            left: 10px;
+            transform: translateY(-50%);
+        }
+        .social-icons a {
+            display: block;
+            margin: 10px 0;
+        }
+        .social-icons img {
+            width: 32px;
+            height: 32px;
+        }
         </style>
         """,
         unsafe_allow_html=True
@@ -62,6 +76,17 @@ def main():
     visitor_count = increment_visitor_count()
     st.markdown(f'<div class="visitor-counter">Total Visitors: {visitor_count}</div>', unsafe_allow_html=True)
     
+    st.markdown(
+        """
+        <div class="social-icons">
+            <a href="https://www.youtube.com" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/4/42/YouTube_icon_%282013-2017%29.png" alt="YouTube"></a>
+            <a href="https://www.facebook.com" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg" alt="Facebook"></a>
+            <a href="https://www.instagram.com" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png" alt="Instagram"></a>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+
     # Initialize session state for inputs
     if "reset" not in st.session_state:
         st.session_state.reset = False
